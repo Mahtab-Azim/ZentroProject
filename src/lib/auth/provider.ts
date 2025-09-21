913afb128edcac6e89fab02fc6e080b7a9e7e1dd
Mahtab <mahtabazim784@gmail.com>
2025-09-21 19:27:21 +0330

    fix the provider and router from NextAuth

--- src/lib/auth/provider.ts
+++ src/lib/auth/provider.ts
@@ -1,13 +1,15 @@
 import GoogleProvider from "next-auth/providers/google"
 import EmailProvider from "next-auth/providers/email"
-import CredentialsProvider from "next-auth/providers/credentials"
-// import { compare } from "bcryptjs"
 
 export const providers = [
-    //login with Google
     GoogleProvider({
         clientId: process.env.GOOGLE_CLIENT_ID!,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-]
+      }),
+      EmailProvider({
+        server: process.env.EMAIL_SERVER!, // مثلا SMTP Gmail یا Mailtrap
+        from: process.env.EMAIL_FROM!,    // ایمیلی که می‌خوای لینک‌ها ازش ارسال بشن
+      }),
+    ]
+    
 
