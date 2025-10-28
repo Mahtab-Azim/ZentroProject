1534a8675c41235e9fee25c0d5c21ca65a46121a
mahtab <mahtabazim784@gmail.com>
2025-10-28 17:05:01 +0330

    Update Next.js, React.js and NextAuth and fix the problems

--- src/app/auth/forgotpassword/page.tsx
+++ src/app/auth/forgotpassword/page.tsx
@@ -17,7 +17,7 @@ export default function ForgotPasswordPage() {
     
     const router = useRouter()
 
-    const handleSubmit = async (e: React.FormEvent) => {
+    const handleSubmit = async (e: React.FormEvent) => { 
         e.preventDefault()
         setIsLoading(true)
         setError("")
